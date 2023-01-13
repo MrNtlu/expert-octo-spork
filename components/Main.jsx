149554a012c707bc34ecbc37138467ac7d20b77e
Burak Fidan <97burakfidan97@gmail.com>
2023-01-13 19:46:12 +0300

    Contact buttons design changed.

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-import ContactIcons from './ContactIcons'
+import { AiFillMail, AiOutlineMedium } from 'react-icons/ai'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
+import Link from 'next/link'
+
 
 const Main = () => {
+    const _iconSize = 27;
+
     return (
         <div className='w-full h-screen text-center'>
             <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
@@ -20,8 +25,44 @@ const Main = () => {
                         Computer Engineering Student and Developer
                     </p>
 
-                    <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
-                        <ContactIcons />
+                    <div className='flex items-center justify-between max-w-[330px] m-auto py-6'>
+                        <div className='flex flex-col gap-4 w-full'>
+                            <a
+                                href='https://www.linkedin.com/in/burak-fidan/'
+                                target='_blank'
+                                rel='noreferrer'>
+                                <div className='flex flex-row rounded-2xl bg-[#5651e5] py-3 px-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                    <div className='row-span-2'><FaLinkedinIn color='#FFFFFF' size={_iconSize} /></div>
+                                    <div className='w-full text-[1.15rem] font-medium content-center text-[#FFFFFF]'>Linkedin</div>
+                                </div>
+                            </a>
+
+                            <a
+                                href='https://github.com/MrNtlu'
+                                target='_blank'
+                                rel='noreferrer'>
+                                <div className='flex flex-row rounded-2xl bg-[#5651e5] shadow-lg shadow-gray-400 py-3 px-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                    <div className='row-span-2'><FaGithub color='#FFFFFF' size={_iconSize} /></div>
+                                    <div className='w-full text-[1.15rem] font-medium content-center text-[#FFFFFF]'>Github</div>
+                                </div>
+                            </a>
+
+                            <a href="mailto:97burakfidan97@gmail.com">
+                                <div className='flex flex-row rounded-2xl bg-[#5651e5] shadow-lg shadow-gray-400 py-3 px-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                    <div className='row-span-2'><AiFillMail color='#FFFFFF' size={_iconSize} /></div>
+                                    <div className='w-full text-[1.15rem] font-medium content-center text-[#FFFFFF]'>Mail</div>
+                                </div>
+                            </a>
+
+                            <Link href='https://burakdev.medium.com/'>
+                                <a>
+                                    <div className='flex flex-row rounded-2xl bg-[#5651e5] shadow-lg shadow-gray-400 py-3 px-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                        <div className='row-span-2'><AiOutlineMedium color='#FFFFFF' size={_iconSize} /></div>
+                                        <div className='w-full text-[1.15rem] font-medium content-center text-[#FFFFFF]'>Medium</div>
+                                    </div>
+                                </a>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
